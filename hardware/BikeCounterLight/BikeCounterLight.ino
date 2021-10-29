da505493146c21fb951a6b7f70da4e205b0a359b
TobiasMeier <meier.tobias@hotmail.com>
2021-10-29 13:19:04 +0200

    BikeCounterLight: Timer changed to "LowPower sleep interval" implementation

--- hardware/BikeCounterLight/BikeCounterLight.ino
+++ hardware/BikeCounterLight/BikeCounterLight.ino
@@ -3,17 +3,15 @@
 #include "arduino_secrets.h"
 
 // set this to activate serial debug messages and to disable deepSleep
-const bool debugFlag = 1;
+const bool debugFlag = 0;
 
 // Threshold for non periodic data transmission
 const int sendThreshold = 10;
+// Sleep intervall (ms)
+const u_int32_t sleepTime = 60000ul; // 4h = 14400000ul (4*60*60*1000)
 
 // Interrupt pins
-const int counterInterruptPin = 0;
-const int timerInterruptPin = 1;
-
-// Timer feedback pin
-const int donePin = 4;
+const int counterInterruptPin = 1;
 
 // lora modem object and application properties
 LoRaModem modem(Serial1);
@@ -23,8 +21,10 @@ String appKey = SECRET_APPKEY;
 // Motion counter value
 // must be volatile as incremented in interrupt
 volatile int counter = 0;
+// motion detected flag
+volatile bool motionDetected = 0;
 // Timer interrupt falg
-bool timerCalled = 0;
+volatile bool timerCalled = 0;
 // Lora data transmission flag
 bool isSending = 0;
 // Error counter for connection
@@ -33,80 +33,90 @@ int errorCounter = 0;
 // Blink methode prototype
 void blinkLED(int times = 1);
 
-void setup() {
+void setup()
+{
   // setup onboard LED
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // setup timer feedback
-  pinMode(donePin, OUTPUT);
-  digitalWrite(donePin, LOW);
-
-  if (debugFlag) {
+  if (debugFlag)
+  {
     // open serial connection and wait
     Serial.begin(9600);
-    while (!Serial);
+    while (!Serial)
+      ;
+    Serial.println("Lora setup started");
   }
 
   // connect to lora network
   doConnect();
 
+  if (debugFlag)
+  {
+    Serial.println("Lora setup finished");
+  }
+
   // setup counter interrupt
   pinMode(counterInterruptPin, INPUT);
   LowPower.attachInterruptWakeup(counterInterruptPin, onMotionDetected, RISING);
-
-  // setup timer interrupt
-  pinMode(timerInterruptPin, INPUT);
-  LowPower.attachInterruptWakeup(timerInterruptPin, onTimerInterrupt, RISING);
-
-  delay(200);
-
-  // clear timer request if allready occurred
-  resetTimer();
 }
 
-void loop() {
-  
-  if (((counter >= sendThreshold) || (timerCalled)) && (!isSending)) {
+void loop()
+{
+  // check if a motion was detected. If not the sleep periode is expired
+  if (motionDetected)
+  {
+    motionDetected = 0;
+    counter++;
+
+    blinkLED();
+
+    if (debugFlag)
+    {
+      Serial.print("Motion detected (current count = ");
+      Serial.print(counter);
+      Serial.println(')');
+    }
+  }
+  else
+  {
+    timerCalled = 1;
+  }
+
+  // send the data if the threshold or the periodic time intervall is exceeded
+  if (((counter >= sendThreshold) || (timerCalled)) && (!isSending))
+  {
+    timerCalled = 0;
     blinkLED(2);
     sendData();
   }
 
-  if (!debugFlag) {
-    delay(200);
-    LowPower.sleep();
+  delay(200);
+  LowPower.sleep(sleepTime);
+}
+
+void onMotionDetected()
+{
+  if (!isSending)
+  {
+    motionDetected = 1;
   }
 }
 
-void onMotionDetected() {
-  if (!isSending) {
-    counter++;
-    blinkLED();
-    if (debugFlag) {
-      Serial.print("Motion detected (current count = ");
-      Serial.print(counter);
-      Serial.println(")");
-    }
-  }
-}
-
-void onTimerInterrupt() {
-  if (!isSending) {
-    timerCalled = 1;
-    if (debugFlag) {
-      Serial.println("Timer called");
-    }
-  }
-}
-
-void doConnect() {
-  if (!modem.begin(EU868)) {
-    if (debugFlag) {
+void doConnect()
+{
+  if (!modem.begin(EU868))
+  {
+    if (debugFlag)
+    {
       Serial.println("Failed to start module");
     }
     blinkLED(5);
-    while (1) {}
+    while (1)
+    {
+    }
   };
-  if (debugFlag) {
+  if (debugFlag)
+  {
     Serial.print("Your module version is: ");
     Serial.println(modem.version());
     Serial.print("Your device EUI is: ");
@@ -114,11 +124,14 @@ void doConnect() {
   }
   delay(4000); //increase up to 10s if connectio does not work
   int connected = modem.joinOTAA(appEui, appKey);
-  if (!connected) {
-    if (debugFlag) {
+  if (!connected)
+  {
+    if (debugFlag)
+    {
       Serial.println("Something went wrong; are you indoor? Move near a window and retry");
     }
-    while (1) {
+    while (1)
+    {
       blinkLED();
     }
   }
@@ -126,7 +139,8 @@ void doConnect() {
   blinkLED(3);
 }
 
-void sendData() {
+void sendData()
+{
   isSending = 1;
   int err;
   //data is transmitted as Ascii chars
@@ -135,27 +149,31 @@ void sendData() {
   payload[0] = lowByte(counter);
   modem.write(payload, sizeof(payload));
   err = modem.endPacket(false); // true = confirmation request
-  if (err > 0) {
-    if (debugFlag) {
+  if (err > 0)
+  {
+    if (debugFlag)
+    {
       Serial.println("Message sent correctly!");
     }
     counter = 0;
-  } else {
-    if (debugFlag) {
+  }
+  else
+  {
+    if (debugFlag)
+    {
       Serial.println("Error sending message :(");
     }
     errorCounter++;
-    if (errorCounter > 1) {
+    if (errorCounter > 1)
+    {
       digitalWrite(LORA_RESET, LOW);
-      if (debugFlag) {
+      if (debugFlag)
+      {
         Serial.println("Trying to reconnect");
       }
       doConnect();
     }
   }
-
-  // reset the timer
-  resetTimer();
 
   // wait for all data transmission to finish
   delay(200);
@@ -163,21 +181,12 @@ void sendData() {
 }
 
 // blinks the on board led
-void blinkLED(int times) {
-  for (int i = 0; i <= times; i++) {
+void blinkLED(int times)
+{
+  for (int i = 0; i <= times; i++)
+  {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(200);
     digitalWrite(LED_BUILTIN, LOW);
-  }
-}
-
-// resets the timer by giving the "done" feedback
-// (Falling edge on done pin)
-void resetTimer () {
-  timerCalled = 0;
-  // generate 3 falling edges to be sure the timer catches it
-  for (int i = 0; i < 3; ++i) {
-    digitalWrite(donePin, HIGH);
-    digitalWrite(donePin, LOW);
   }
 }
