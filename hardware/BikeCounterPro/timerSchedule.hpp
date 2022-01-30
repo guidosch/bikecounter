5ebef1994c7846b627f1793920a0d896bace3adf
TobiasMeier <meier.tobias@hotmail.com>
2022-01-30 22:19:08 +0100

    timerSchedule class finished

--- hardware/BikeCounterPro/timerSchedule.hpp
+++ hardware/BikeCounterPro/timerSchedule.hpp
@@ -7,14 +7,18 @@ class TimerSchedule
 {
 public:
     TimerSchedule();
-    TimeSpan getCurrentTimerIntervall(DateTime currentDateTime);
+    DateTime getNextIntervalTime(DateTime &currentDateTime);
 
 private:
-    TimeSpan shortInterval = TimeSpan(0, 2, 0, 0);
-    TimeSpan longInterval = TimeSpan(0, 6, 0, 0);
-    int startHourShortInterval [12] = { 8,  7,  6,  6,  6,  6,  6,  6,  7,  7,  7,  8};
-    int startHourLongtInterval [12] = {17, 18, 20, 21, 22, 22, 22, 21, 20, 19, 17, 17};
-    bool isLastInterval(DateTime currentDateTime);
+    static const int intervalCount = 3;
+    TimeSpan timeSpanIntervals[intervalCount] = {TimeSpan(0, 6, 0, 0), TimeSpan(0, 2, 0, 0), TimeSpan(0, 6, 0, 0)}; // {dayInterval, nightInterval}
+    int IntervalStartUTC[intervalCount][12] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {7, 6, 5, 5, 5, 5, 5, 5, 6, 6, 6, 7},              // ZRH (+1) { 8,  7,  6,  6,  6,  6,  6,  6,  7,  7,  7,  8} start day interval
+        {16, 17, 19, 20, 21, 21, 21, 20, 19, 18, 16, 16}}; // ZRH (+1) {17, 18, 20, 21, 22, 22, 22, 21, 20, 19, 17, 17} start night interval
+    bool isLastCall = false;
+    TimeSpan offset = TimeSpan(0, 0, 10, 0);
+    int getIntervalId(int currentHour, int currentMonth);
 };
 
 #endif //TIMERSCHEDULE_H
