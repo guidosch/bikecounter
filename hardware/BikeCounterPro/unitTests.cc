33889de10991427190c48093f07e189a596b76e1
TobiasMeier <meier.tobias@hotmail.com>
2022-02-13 13:55:27 +0100

    gtest fixture and dependent libraries added

--- hardware/BikeCounterPro/unitTests.cc
+++ hardware/BikeCounterPro/unitTests.cc
@@ -1,6 +1,32 @@
 #include <gtest/gtest.h>
+#include "timerSchedule.hpp"
 
-TEST(UnitTests, BasicAssertions)
+class TimerScheduleTest : public ::testing::Test
+{
+protected:
+    TimerSchedule t1;
+
+    // void SetUp() override {}
+    // void TearDown() override {}
+};
+
+TEST_F(TimerScheduleTest, BasicIntervalTests)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST_F(TimerScheduleTest, LastCallInIntervalTests)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST_F(TimerScheduleTest, LastCallOfTheDayTests)
 {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
