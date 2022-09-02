72e523f7cbc8326c97aea1c5a79d94d7be7b3148
TobiasMeier <meier.tobias@hotmail.com>
2022-09-02 22:46:27 +0200

    time synchronization properties to payload formatter added

--- hardware/BikeCounterPro/src/dataPackage/payloadFormatter_downlink.js
+++ hardware/BikeCounterPro/src/dataPackage/payloadFormatter_downlink.js
@@ -1,7 +1,5 @@
 function encodeDownlink(input) {
-  var today = new Date();
-  // seconds since 1 Jan 1970
-  var seconds = (today.getTime() / 1000) >> 0;
+  var seconds = input.data.timeDrift >> 0;
   var encodedSeconds = [0, 0, 0, 0];
   encodedSeconds[0] = seconds & 0xff;
   encodedSeconds[1] = (seconds >> 8) & 0xff;
