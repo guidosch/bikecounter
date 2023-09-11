2ecfeab05f1ed2f293e86c9af6dca5ba54013872
Tobias Meier <meier.tobias@hotmail.com>
2023-09-11 10:35:42 +0200

    setup method moved

--- software/BikeCounterPro/src/bikeCounter/bikeCounter.cpp
+++ software/BikeCounterPro/src/bikeCounter/bikeCounter.cpp
@@ -31,6 +31,125 @@ void BikeCounter::reset()
 
 void BikeCounter::setup()
 {
+    // read dip switch states
+    pinMode(debugSwitchPin, INPUT);
+    pinMode(configSwitchPin, INPUT);
+    debugFlag = digitalRead(debugSwitchPin);
+    configFlag = digitalRead(configSwitchPin);
+
+    // deactivate the dip switch pins
+    pinMode(debugSwitchPin, OUTPUT);
+    pinMode(configSwitchPin, OUTPUT);
+    digitalWrite(debugSwitchPin, LOW);
+    digitalWrite(configSwitchPin, LOW);
+
+    // setup pin modes
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(pirPowerPin, OUTPUT);
+    disableUnusedPins();
+    blinkLED(2);
+
+    // disable the pir sensor
+    digitalWrite(pirPowerPin, LOW);
+
+    // The MKR WAN 1310 3.3V reference voltage for battery measurements
+    analogReference(AR_DEFAULT);
+
+    // initialize the I2C communication
+    Wire.begin();
+
+    // initialize the logging instance
+    StatusLogger::Output outputType = debugFlag ? StatusLogger::Output::toSerial : StatusLogger::Output::noOutput;
+    logger.setup(outputType);
+
+    // load config from flash
+    logger.push("Load config from flash");
+    logger.loop();
+    // LORA reset pin declaration as output
+    pinMode(LORA_RESET, OUTPUT);
+    // turn off LORA module to not interrupt the flash communication
+    digitalWrite(LORA_RESET, LOW);
+    delay(500);
+    // begin flash communication
+    if (flash.begin(PIN_FLASH_CS, 2000000, SPI1) == false)
+    {
+        logger.push("SPI Flash not detected");
+        logger.loop();
+
+        currentStatus = error;
+        return;
+    }
+
+    // first byte in memory contains the length of the config array
+    uint8_t readSize = flash.readByte(0);
+    // read buffer
+    uint8_t rBuffer[255];
+    // read bytes from flash
+    flash.readBlock(1, rBuffer, readSize);
+    // cast buffer to string array
+    String config = String((char *)rBuffer);
+    // split and assign config string
+    String appEui = config.substring(config.indexOf(':') + 1, config.indexOf(';'));
+    String appKey = config.substring(config.indexOf(';') + 1).substring(config.indexOf(':') + 1);
+    // digitalWrite(LORA_RESET, HIGH);
+    logger.push(String("appEui = ") + appEui);
+    logger.push(String("appKey = ") + appKey);
+    logger.push("Temp. sensor setup started");
+    logger.loop();
+
+    delay(500);
+
+    
+  // initialize temperature and humidity sensor
+  am2320.begin();
+
+  logger.push("Temp. sensor setup finished");
+  logger.push("Lora setup started");
+  logger.loop();
+
+  delay(500);
+
+  // connect to lora network
+  loRaConnector.setup(appEui, appKey, logger);
+  while (loRaConnector.getStatus() != LoRaConnector::Status::connected)
+  {
+    loRaConnector.loop();
+    delay(100);
+  }
+
+  logger.push("Lora setup finished");
+  logger.push("RTC setup started");
+  logger.loop();
+
+  // setup rtc
+  rtc.begin(true);
+  rtc.setEpoch(defaultRTCEpoch);
+
+  logger.push(String("RTC current time: ") +
+              String(rtc.getHours()) +
+              String(':') +
+              String(rtc.getMinutes()) +
+              String(':') +
+              String(rtc.getSeconds()));
+  logger.push(String("RTC current date: ") +
+              String(rtc.getDay()) +
+              String('.') +
+              String(rtc.getMonth()) +
+              String('.') +
+              String(rtc.getYear()));
+  logger.push(String("RTC epoch: ") +
+              String(rtc.getEpoch()));
+  logger.loop();
+
+  // delay to avoid interference with interrupt pin setup
+  delay(200);
+
+  // setup counter interrupt
+  pinMode(counterInterruptPin, INPUT_PULLDOWN);
+  LowPower.attachInterruptWakeup(counterInterruptPin, onMotionDetected, RISING);
+
+  logger.push("Setup finished");
+  logger.loop();
 }
 
 void BikeCounter::blinkLED(int times)
--- software/BikeCounterPro/src/bikeCounter/bikeCounter.hpp
+++ software/BikeCounterPro/src/bikeCounter/bikeCounter.hpp
@@ -2,6 +2,16 @@
 #define BIKECOUNTER_H
 
 #include <Arduino.h>
+#include <RTCZero.h>
+#include <ArduinoLowPower.h>
+#include <Adafruit_AM2320.h>
+#include <SPI.h>
+#include <SparkFun_SPI_SerialFlash.h>
+#include "../statusLogger/statusLogger.hpp"
+#include "../LoRaConnector/LoRaConnector.hpp"
+#include "../dataPackage/dataPackage.hpp"
+#include "../timerSchedule/timerSchedule.hpp"
+#include "../timerSchedule/date.h"
 
 class BikeCounter
 {
@@ -48,7 +58,7 @@ public:
     void setSyncTimeInterval(uint32_t ms) { syncTimeInterval = ms; }
     /// @brief Deactivate the onboard LED after the specified amount of blinks
     /// @param count
-    void setMaxBlink(int count) { maxBlinks = count; }
+    void setMaxBlinks(int count) { maxBlinks = count; }
     /// @brief Max. counts between timer calls (to detect a floating interrupt pin)
     /// @param count
     void setMaxCount(int count) { maxCount = count; }
@@ -66,6 +76,62 @@ private:
     int maxCount;
     int maxBlinks;
 
+    // Object to log the status of the device
+    StatusLogger logger = StatusLogger();
+
+    // Object to handel all the LoRa stuff
+    LoRaConnector loRaConnector = LoRaConnector();
+
+    // Internal RTC object
+    RTCZero rtc;
+
+    // Humidity and temperature sensor object
+    Adafruit_AM2320 am2320 = Adafruit_AM2320();
+
+    // DataPackage object to encode the payload
+    DataPackage dataHandler = DataPackage();
+
+    // TimerSchedule object to determine the next timer call
+    TimerSchedule timeHandler = TimerSchedule();
+
+    // Motion counter value
+    int counter = 0;
+    // total counts between timer calls
+    int totalCounter = 0;
+    // motion detected flag (must be volatile as changed in IRS)
+    volatile bool motionDetected = 0;
+    // time array size
+    static const int timeArraySize = 62;
+    // time array
+    unsigned int timeArray[timeArraySize];
+    // hour of the day for next package
+    unsigned int hourOfDay = 0;
+    // Lora data transmission flag (must be volatile as changed in IRS)
+    volatile bool isSending = 0;
+    // Error counter for connection
+    int errorCounter = 0;
+    // Error counter for pir-sensor
+    int pirError = 0;
+    // Holds the debug state of the dip switch
+    int debugFlag = 0;
+    // Holds the config state of the dip switch
+    int configFlag = 0;
+    // Last call of main loop in debug mode
+    unsigned long lastMillis = millis() - 10 * 60 * 1000;
+    // default startup date 01.01.2022 (1640995200)
+    uint32_t defaultRTCEpoch = 1640995200ul;
+    // Time sync skip flag (Prevents that the time correction is applied multiple times due to network lag and multiple enqueued downlinks with the same timeDrift information)
+    int skipTimeSync = 0;
+    // SPI serial flash parameter
+    const byte PIN_FLASH_CS = 32;
+    // SPI serial flash object
+    SFE_SPI_FLASH flash;
+    // Flags to trigger the initial sleep period and reset the rtc (prevent rtc bug)
+    int firstLoop = 1;
+    int firstWakeUp = 1;
+    // Next wakeup time (epoch)
+    std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds> nextAlarm{std::chrono::seconds{0}};
+
     Status currentStatus = setupStep;
     void setup();
     /// @brief blinks the on-board led
@@ -76,6 +142,9 @@ private:
     /// @brief reads the analog value and calculates the battery voltage.
     /// @return Battery voltage
     float getBatteryVoltage();
+    /// @brief Blink method prototype
+    /// @param times to blink
+    void blinkLED(int times = 1);
 };
 
 #endif // BIKECOUNTER_H
