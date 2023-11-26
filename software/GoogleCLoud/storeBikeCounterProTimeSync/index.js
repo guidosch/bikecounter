66f089072e4c0f429aed50be5ee29233442a6e71
TobiasMeier <meier.tobias@hotmail.com>
2023-11-26 02:24:23 +0100

    connection Table query added

--- software/GoogleCLoud/storeBikeCounterProTimeSync/index.js
+++ software/GoogleCLoud/storeBikeCounterProTimeSync/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions"),
-admin = require("firebase-admin");
+  admin = require("firebase-admin");
 const https = require("https");
 
 const app = admin.initializeApp();
 const firestore = app.firestore();
+const db = admin.firestore();
 var num = 0;
 
 firestore.settings({ timestampsInSnapshots: true });
@@ -12,12 +13,13 @@ const auth = app.auth();
 const ttnWebhookId = "google-cloud-function";
 
 exports.storeBikecounterData = (req, res) => {
-  
   let payload = req.body;
   let deviceId;
+  let deviceEUI;
   const app_id = payload.end_device_ids.application_ids.application_id;
   if (payload) {
     deviceId = payload.end_device_ids.device_id;
+    deviceEUI = payload.end_device_ids.dev_eui;
     const devicePayload = payload.uplink_message.decoded_payload;
     const batteryLevel = devicePayload.batteryLevel;
     const batteryVoltage = devicePayload.batteryVoltage;
@@ -49,40 +51,63 @@ exports.storeBikecounterData = (req, res) => {
 
     // LoRa network informations
     let gateways = [];
-    for (let gateway of payload.uplink_message.rx_metadata) { 
-      gateways.push({id: gateway.gateway_ids.gateway_id,
-                  eui: gateway.gateway_ids.eui,
-                  rssi: gateway.rssi,
-                  snr: gateway.snr });
+    for (let gateway of payload.uplink_message.rx_metadata) {
+      gateways.push({
+        id: gateway.gateway_ids.gateway_id,
+        eui: gateway.gateway_ids.eui,
+        rssi: gateway.rssi,
+        snr: gateway.snr,
+      });
     }
     const airtime = payload.uplink_message.consumed_airtime;
 
     // statId == 7 is the time sync call
     if (app_id == "bikecounter" && statId != 7) {
       try {
-        firestore.collection(`${deviceId}`).add({
-          counter: 0,
-          timestamp: new Date(transmissionTime).toISOString(),
-          batteryLevel: batteryLevel,
-          batteryVoltage: batteryVoltage,
-          humidity: humidity,
-          temperature: temperature,
-          stat: stat,
-          gateways: gateways,
-          airtime: airtime,
-          swVersion: swVersion,
-          hwVersion: hwVersion,
-        });
-        console.log(`Added health data for ${deviceId}`);
+        // get the collection id (trail) from the deviceEUI
+        db.collection("internal-deviceId-trail-ct")
+          .where("deviceEUI", "==", deviceEUI)
+          .orderBy("validFrom")
+          .limit(1)
+          .get()
+          .then((snapshot) => {
+            if (snapshot.empty) {
+              console.log("No trail for device found!");
+            } else {
+              snapshot.forEach((doc) => {
+                console.log(doc.data().collectionID);
+                const collId = doc.data().collectionID;
 
-        //one more DB entry for every timestamp
-        for (let timestamp of map.keys()) {
-          let date = new Date(timestamp).toISOString();
-          firestore
-            .collection(`${deviceId}`)
-            .add({ counter: map.get(timestamp), timestamp: date });
-          console.log(`Added data for ${deviceId}`);
-        }
+                // store the parsed payload into the trail collection
+                firestore.collection(`${collId}`).add({
+                  counter: 0,
+                  timestamp: new Date(transmissionTime).toISOString(),
+                  batteryLevel: batteryLevel,
+                  batteryVoltage: batteryVoltage,
+                  humidity: humidity,
+                  temperature: temperature,
+                  stat: stat,
+                  gateways: gateways,
+                  airtime: airtime,
+                  swVersion: swVersion,
+                  hwVersion: hwVersion,
+                });
+                console.log(`Added health data for ${collId}`);
+
+                //one more DB entry for every timestamp
+                for (let timestamp of map.keys()) {
+                  let date = new Date(timestamp).toISOString();
+                  firestore
+                    .collection(`${collId}`)
+                    .add({ counter: map.get(timestamp), timestamp: date });
+                  console.log(`Added data for ${collId}`);
+                }
+              });
+            }
+          })
+          .catch((err) => {
+            console.error("Error while performing queries! ", err);
+          });
       } catch (error) {
         console.error(
           `error while trying to store data for: ${deviceId}`,
