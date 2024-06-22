f7d52a62d1b949dab4e185129eb66477657e2d7d
TobiasMeier <meier.tobias@hotmail.com>
2024-06-22 18:34:34 +0200

    sw and hw version updated

--- software/BikeCounterPro/config.h
+++ software/BikeCounterPro/config.h
@@ -1,7 +1,7 @@
 #ifndef BIKECOUNTER_CONFIG_H
 #define BIKECOUNTER_CONFIG_H
 
-const uint8_t hwVersion = 2;
-const uint8_t swVersion = 5;
+const uint8_t hwVersion = 4;
+const uint8_t swVersion = 6;
 
 #endif // BIKECOUNTER_CONFIG_H
