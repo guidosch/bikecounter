1268cd47b3ae87bc371d4baa91f896d76a5c1e7a
Tobias Meier <meier.tobias@hotmail.com>
2026-01-06 21:16:23 +0100

    Refactor downlink request example to use Swisscom API and encode time drift payload

--- software/GoogleCLoud/storeBikeCounterProTimeSync/downlinkRequestExample.js
+++ software/GoogleCLoud/storeBikeCounterProTimeSync/downlinkRequestExample.js
@@ -1,31 +1,36 @@
 const https = require("https");
 
 const bearerToken = "xxx";
-const appId = "bikecounter-dev";
-const webhookId = "google-cloud-function";
-const deviceId = "test-device-1";
+const deviceId = "A8610A32314B7505";
+const timeDrift = -2587261;
 
-const data = JSON.stringify({
-  downlinks: [
-    {
-      decoded_payload: {
-        timeDrift: -2587261,
-      },
-    },
-  ],
-});
+function encodeDownlink(timeDrift) {
+  var seconds = timeDrift >> 0;
+  var encodedSeconds = [0, 0, 0, 0];
+  encodedSeconds[0] = seconds & 0xff;
+  encodedSeconds[1] = (seconds >> 8) & 0xff;
+  encodedSeconds[2] = (seconds >> 16) & 0xff;
+  encodedSeconds[3] = (seconds >> 24) & 0xff;
+  encodedSecondsHexString = encodedSeconds.map(b => b.toString(16).padStart(2, '0')).join('');
+  
+  console.log("Encoded seconds hex string: " + encodedSecondsHexString);
+
+  return JSON.stringify({
+    "DevEUI_downlink": {
+      "Time": new Date().toISOString(),
+      "DevEUI": deviceId,
+      "FPort": 1,
+      "payload_hex": encodedSecondsHexString
+    }
+  });
+}
+
+const data = encodeDownlink(timeDrift);
 
 const options = {
-  hostname: "eu1.cloud.thethings.network",
+  hostname: "portal.lpn.swisscom.ch",
   port: 443,
-  path:
-    "/api/v3/as/applications/" +
-    appId +
-    "/webhooks/" +
-    webhookId +
-    "/devices/" +
-    deviceId +
-    "/down/replace",
+  path: "/thingpark/lrc/rest/v2/downlink/",
   method: "POST",
   headers: {
     Authorization: "Bearer " + bearerToken,
